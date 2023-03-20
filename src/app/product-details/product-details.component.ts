fb94ebd51a3769c10b9843eb1c73626b998cc5a0
Abdulateef Al-radaee <abdulateef@onegr.com>
2023-03-20 13:54:50 +0300

    learning Angular

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products, Product } from '../products';
+import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute, private cartServie: CartService) {
+  };
 
+  ngOnInit() {
+    const currentRoute = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(currentRoute.get('productId'))
+    this.product = products.find( product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartServie.addToCart(product);
+    window.alert(product.name + ' added to cart');
+  }
 }
